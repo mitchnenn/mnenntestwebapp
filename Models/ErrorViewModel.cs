7dcbfcf79ca952e36c143b381c62cf7e31cadd48
Michael Nenn <mnenn@illumina.com>
2020-06-09 20:54:58 -0700

    Initial check in.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MnennTestWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
